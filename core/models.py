1aa27f56b8b0aec3bebe7526062adb98d0bcf1c6
Coen van der Kamp <coen@fourdigits.nl>
2020-07-29 21:04:49 +0200

    Add links to Wagtail source code

--- core/models.py
+++ core/models.py
@@ -7,37 +7,49 @@ from django.template.defaultfilters import slugify
 from treebeard.mp_tree import MP_Node
 
 
+# https://github.com/wagtail/wagtail/blob/master/wagtail/core/models.py#L186
 class Site(models.Model):
     name = models.CharField(max_length=50)
     host = models.CharField(max_length=50)
     root_page = models.ForeignKey("Page", on_delete=models.PROTECT)
 
 
+# MP_Node https://github.com/wagtail/wagtail/blob/master/wagtail/core/models.py#L375
+# Page https://github.com/wagtail/wagtail/blob/master/wagtail/core/models.py#L388
 class Page(MP_Node):
     title = models.CharField(max_length=50)
     slug = models.SlugField()
+    # Content type https://github.com/wagtail/wagtail/blob/master/wagtail/core/models.py#L405
     content_type = models.ForeignKey(ContentType, related_name="pages", on_delete=models.PROTECT)
 
+    # Set content type https://github.com/wagtail/wagtail/blob/master/wagtail/core/models.py#L529
     def __init__(self, *args, **kwargs):
         super(Page, self).__init__(*args, **kwargs)
         if not self.id:
             self.content_type = ContentType.objects.get_for_model(self)
+        # Not handled in Python, but via frontend / Javascript
         self.slug = slugify(self.title)
 
     def __str__(self):
         return self.title
 
+    # https://github.com/wagtail/wagtail/blob/master/wagtail/core/models.py#L2934
     @property
     def specific(self):
         content_type = ContentType.objects.get_for_id(self.content_type_id)
         return content_type.get_object_for_this_type(id=self.id)
 
+    # https://github.com/wagtail/wagtail/blob/master/wagtail/core/models.py#L1000
     def get_context(self):
         return {"page": self}
 
+    # https://github.com/wagtail/wagtail/blob/master/wagtail/core/models.py#L1018
     def serve(self, request):
+        # Template name https://github.com/wagtail/wagtail/blob/master/wagtail/core/models.py#L357
+        # Get context https://github.com/wagtail/wagtail/blob/master/wagtail/core/models.py#L1000
         return render(request, f"core/{self.__class__.__name__.lower()}.html", self.get_context())
 
+    # https://github.com/wagtail/wagtail/blob/master/wagtail/core/models.py#L848
     def route(self, request, slugs):
         if slugs:
             child_slug = slugs[0]
@@ -51,6 +63,7 @@ class Page(MP_Node):
             return self.serve(request)
 
 
+# Project code, not in Wagtail source
 class HomePage(Page):
     intro = models.TextField()
 
@@ -60,10 +73,12 @@ class HomePage(Page):
         return ctx
 
 
+# Project code, not in Wagtail source
 class ListPage(Page):
     pass
 
 
+# Project code, not in Wagtail source
 class DetailPage(Page):
     author = models.CharField(max_length=50)
     body = models.TextField()
--- core/urls.py
+++ core/urls.py
@@ -3,12 +3,18 @@ from django.urls import re_path
 from core.models import Site
 
 
+# serve 'view' https://github.com/wagtail/wagtail/blob/master/wagtail/core/views.py#L10
 def serve(request, path):
+    # Get site https://github.com/wagtail/wagtail/blob/master/wagtail/core/views.py#L12
     site = Site.objects.get(host=request.get_host())
+    # Split https://github.com/wagtail/wagtail/blob/master/wagtail/core/views.py#L16
     slugs = [slug for slug in path.split("/") if slug]
+    # Call page route https://github.com/wagtail/wagtail/blob/master/wagtail/core/views.py#L24
     return site.root_page.specific.route(request, slugs)
 
 
 urlpatterns = [
+    # Regex https://github.com/wagtail/wagtail/blob/master/wagtail/core/urls.py#L13
+    # re_path https://github.com/wagtail/wagtail/blob/master/wagtail/core/urls.py#L37
     re_path(r"^((?:[\w\-]+/)*)$", serve)
 ]
